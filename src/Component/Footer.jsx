cc2714006a85daf84d38f3a7ffd846d27930f624
elysion-yb24 <yash@elysionsoftwares.in>
2025-02-07 15:41:09 +0530

    fix:sobhagya store link

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -50,7 +50,9 @@ NEAR NAI SADAK, Meerut, Meerut, Uttar Pradesh, 250002
                 <Link to="/termsOfService">TERMS OF SERVICES</Link>
                 <Link to="/returnRefundPolicy">RETURN AND REFUND POLICY</Link>
                 <Link to="/shippingPolicy" >SHIPPING POLICY</Link>
-                <a href='/store.sobhagya.in'>Sobhagya Store</a>
+                <a href="https://store.sobhagya.in" target="_blank" rel="noopener noreferrer">
+        SOBHAGYA STORE
+      </a>
               </div>
               <div className="flex items-center gap-2">
                 <a href="https://www.facebook.com/profile.php?id=61551098336719">
